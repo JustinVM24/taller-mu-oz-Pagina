aa2165b5bbae1c1158a6ea8da25bd1c4b7f85ee4
JustinVM24 <jus4221izp@gmail.com>
2026-05-13 15:13:48 -0700

    main

--- src/app/core/models/empleado.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Empleado {
-  id?: number;
-  nombre: string;
-  email: string;
-  rol: 'dueño' | 'empleado';
-  activo: boolean;
-}
--- src/app/core/models/vehiculo.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Vehiculo {
-  id?: number;
-  placa: string;
-  propietario: string;
-  modelo: string;
-  anio: number;
-  telefono?: string;
-  tieneBorrador?: boolean;
-}
